1bac9e2ffd099d10d494b4292c9856e663225e4e
Mariko <maripiyok@gmail.com>
2015-10-28 08:02:40 +0900

    フォルダ、子フォルダ、子ファイルのルーティングを追加。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
+
+  resources :folders, only: [ :show ] do
+    resources :child_folders, except: [ :show, :index ]
+    resources :uploaded_files, except: [ :show, :index ]
+  end
+
 end
