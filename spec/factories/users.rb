d17023eed8d93a0a88ad4eb4262bdb1df378d41f
Mariko <maripiyok@gmail.com>
2015-10-28 21:16:04 +0900

    Add User spec.
    user_name must be present.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
   factory :user do
-    
+    user_name { Faker::Name::name }
+    email { Faker::Internet::email }
+    password "secret123"
+    password_confirmation "secret123"
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should respond valid factory" do
+    expect(FactoryGirl.build(:user)).to be_valid
+  end
+
+  it "should invalid if no name" do
+    user = FactoryGirl.build(:user, { user_name: nil })
+    expect(user.valid?).to be_falsey
+  end
+  
 end
