383868ebbe45f2195608476b197ab38e9a7894c0
Mariko <maripiyok@gmail.com>
2015-10-28 21:01:47 +0900

    Create Folder model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151015121615) do
+ActiveRecord::Schema.define(version: 20151027230542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "folders", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id",          null: false
+    t.integer  "parent_folder_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  add_index "folders", ["name", "user_id", "parent_folder_id"], name: "index_folders_on_name_and_user_id_and_parent_folder_id", unique: true, using: :btree
+  add_index "folders", ["user_id"], name: "index_folders_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -35,4 +46,6 @@ ActiveRecord::Schema.define(version: 20151015121615) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "folders", "folders", column: "parent_folder_id"
+  add_foreign_key "folders", "users"
 end
---
+++ spec/factories/folders.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :folder do
+    name "MyString"
+user nil
+parent_folder_id 1
+  end
+
+end
